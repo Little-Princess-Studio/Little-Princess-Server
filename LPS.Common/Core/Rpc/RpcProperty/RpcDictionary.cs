73f082e640789653f89284d7f644a8dcf894e261
Huisama <yuwenhusiama@outlook.com>
2022-01-20 00:36:18 +0800

    [feat] add RpcContainerNode

---
+++ LPS.Common/Core/Rpc/RpcProperty/RpcDictionary.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+
+namespace LPS.Core.Rpc.RpcProperty
+{
+    public class RpcDictionary<TK, TV> : RpcProperty<Dictionary<TK, RpcContainerNode<TV>>>
+        where TK : notnull
+    {
+        public Action<TK, TV>? OnElemSet { get; set; }
+        public Action<TK, TV>? OnElemRemoved { get; set; }
+        public Action<TK, TV>? OnNewElemAppended { get; set; }
+        
+        // the container of this RpcDictionary
+        public RpcContainerNode Container { get; set; }
+
+        public RpcDictionary(string name, RpcPropertySetting setting) : base(name, setting, new())
+        {
+        }
+
+        public TV this[TK key]
+        {
+            get => value_[key].Value;
+            set
+            {
+                if (value_.ContainsKey(key))
+                {
+                    value_[key].Value = value;
+                }
+                else
+                {
+                    value_[key] = new RpcContainerNode<TV>(value)
+                    {
+                    };
+                }
+            }
+        }
+
+        public void Clear() => value_.Clear();
+        public bool Remove(TK key) => value_.Remove(key);
+        public ReadOnlyDictionary<TK, RpcContainerNode<TV>> AsReadOnly() => new(value_);
+    }
+
+    public class ShadowRpcDictionary<TK, TV> : ShadowRpcProperty<Dictionary<TK, RpcContainerNode<TV>>>
+        where TK : notnull
+    {
+        public ShadowRpcDictionary(string name) : base(name)
+        {
+            // this.GetType().GetProperty("111").SetMethod.GetMethodBody().
+            Expression<Func<int>> add = () => 1 + 2;
+            var func = add.Compile(); // Create Delegate
+            // func.Method.GetMethodBody()
+        }
+        
+        public TV this[TK key]
+        {
+            get => value_[key].Value;
+            set => throw new Exception("ShadowRpcDictionary is readonly.");
+        }
+        
+        public ReadOnlyDictionary<TK, RpcContainerNode<TV>> AsReadOnly() => new(value_);
+    }
+}
---
+++ LPS.Common/Core/Rpc/RpcProperty/RpcList.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LPS.Core.Rpc.RpcProperty
+{
+    public class RpcList<TElem> : RpcProperty<List<RpcContainerNode<TElem>>>
+    {
+        public RpcList(string name, RpcPropertySetting setting, List<TElem> value) : base(name, setting, new())
+        {
+        }
+
+        public void Clear()
+        {
+            value_.Clear();
+        }
+
+        public void RemoveAt(int index) => value_.RemoveAt(index);
+
+        public void Resize(int size, [DisallowNull] TElem defaultValue)
+        {
+            ArgumentNullException.ThrowIfNull(defaultValue);
+
+            if (value_.Count > 0)
+            {
+                throw new Exception("Resize can only used on empty RpcList");
+            }
+
+            for (int i = 0; i < size; ++i)
+            {
+                value_.Add(new RpcContainerNode<TElem>(defaultValue));
+            }
+        }
+
+        public void Add([DisallowNull] TElem value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            value_.Add(new RpcContainerNode<TElem>(value));
+        }
+
+        public TElem this[int index]
+        {
+            get => value_[index].Value;
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                value_[index].Value = value;
+            }
+        }
+        
+        public ReadOnlyCollection<RpcContainerNode<TElem>> AsReadOnly() => value_.AsReadOnly();
+    }
+
+    public class ShadowRpcList<TElem> : ShadowRpcProperty<List<RpcContainerNode<TElem>>>
+    {
+        public ShadowRpcList(string name) : base(name)
+        {
+        }
+
+        public TElem this[int index]
+        {
+            get => value_[index].Value;
+            set => throw new Exception("RpcList is readonly");
+        }
+
+        public ReadOnlyCollection<RpcContainerNode<TElem>> AsReadOnly() => value_.AsReadOnly();
+    }
+}
