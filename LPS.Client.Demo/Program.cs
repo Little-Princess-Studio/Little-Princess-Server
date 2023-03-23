a5b3c4ca9c1da297538e921175f3b8300218e305
Huisama <yuwenhuisama@outlook.com>
2023-03-24 02:39:01 +0800

    [chore] add startup manager for client

--- LPS.Client.Demo/Program.cs
+++ LPS.Client.Demo/Program.cs
@@ -6,14 +6,7 @@
 
 namespace LPS.Client.Demo;
 
-using Google.Protobuf;
 using LPS.Client.Demo.Console;
-using LPS.Client.Rpc;
-using LPS.Common.Debug;
-using LPS.Common.Rpc;
-using LPS.Common.Rpc.InnerMessages;
-using LPS.Common.Rpc.InnerMessages.ProtobufDefs;
-using Client = LPS.Client.Client;
 
 /// <summary>
 /// Client entry class.
@@ -28,27 +21,22 @@ public class Program
     /// <param name="args">Entry args.</param>
     public static void Main(string[] args)
     {
-        Logger.Init("client");
-        RpcProtobufDefs.Init();
-        RpcHelper.ScanRpcMethods("LPS.Client.Entity");
-        RpcHelper.ScanRpcPropertyContainer("LPS.Client.Entity.RpcProperties");
-        CommandParser.ScanCommands("LPS.Client");
+        CommandParser.ScanCommands("LPS.Client.Demo");
 
-        Client.Instance.Init("127.0.0.1", 11001);
+        StartUpManager.Init(
+            "127.0.0.1",
+            11001,
+            "LPS.Client.Demo.Entity",
+            "LPS.Client.Demo.Entity.RpcProperty",
+            () => ClientGlobal.ShadowClientEntity,
+            entity => ClientGlobal.ShadowClientEntity = entity);
 
-        // Client.Instance.Init("52.175.74.209", 11001);
-        Client.Instance.RegisterMessageHandler(PackageType.ClientCreateEntity, HandleClientCreateEntity);
-        Client.Instance.RegisterMessageHandler(PackageType.EntityRpc, HandleEntityRpc);
-        Client.Instance.RegisterMessageHandler(PackageType.PropertyFullSync, HandlePropertyFullSync);
-        Client.Instance.RegisterMessageHandler(PackageType.PropertySyncCommandList, HandlePropertySyncCommandList);
-
-        Client.Instance.Start();
+        StartUpManager.StartClient();
 
         AutoCompleteConsole.Init();
         AutoCompleteConsole.Loop();
 
-        Client.Instance.Stop();
-        Client.Instance.WaitForExit();
+        StartUpManager.StopClient();
     }
 
     private static string RandomString(int length)
@@ -56,71 +44,5 @@ public class Program
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         return new string(Enumerable.Repeat(chars, length)
             .Select(s => s[Random.Next(s.Length)]).ToArray());
-    }
-
-    private static void HandlePropertySyncCommandList(object arg)
-    {
-        var (msg, _, _) = ((IMessage, Connection, uint))arg;
-        var syncCommandList = (msg as PropertySyncCommandList) !;
-
-        Logger.Debug($"[HandlePropertySyncCommandList] {msg}");
-
-        ClientGlobal.ShadowClientEntity.ApplySyncCommandList(syncCommandList);
-    }
-
-    private static void HandleEntityRpc(object arg)
-    {
-        var (msg, _, _) = ((IMessage, Connection, uint))arg;
-        var entityRpc = (EntityRpc)msg;
-
-        // Logger.Info($"rpc msg from server {entityRpc}");
-        RpcHelper.CallLocalEntity(ClientGlobal.ShadowClientEntity, entityRpc);
-    }
-
-    private static void HandleClientCreateEntity(object arg)
-    {
-        var (msg, _, _) = ((IMessage, Connection, uint))arg;
-        var clientCreateEntity = (ClientCreateEntity)msg;
-
-        Logger.Info(
-            $"Client create entity: {clientCreateEntity.EntityClassName} " +
-            $"{clientCreateEntity.ServerClientMailBox}");
-
-        var shadowEntity = RpcClientHelper.CreateClientEntity(clientCreateEntity.EntityClassName);
-        shadowEntity.OnSend = rpc => { Client.Instance.Send(rpc); };
-        shadowEntity.MailBox = RpcHelper.PbMailBoxToRpcMailBox(clientCreateEntity.ServerClientMailBox);
-        shadowEntity.BindServerMailBox();
-
-        ClientGlobal.ShadowClientEntity = shadowEntity;
-        Logger.Info($"{shadowEntity} created success.");
-
-        var requireFullSync = new RequirePropertyFullSync()
-        {
-            EntityId = shadowEntity.MailBox.Id,
-        };
-
-        Client.Instance.Send(requireFullSync);
-        Logger.Info($"require full property sync");
-    }
-
-    private static void HandlePropertyFullSync(object arg)
-    {
-        var (msg, _, _) = ((IMessage, Connection, uint))arg;
-        var propertyFullSyncMsg = (PropertyFullSync)msg;
-
-        if (propertyFullSyncMsg.EntityId != ClientGlobal.ShadowClientEntity.MailBox.Id)
-        {
-            throw new Exception(
-                $"Invalid property full sync {propertyFullSyncMsg.EntityId} {ClientGlobal.ShadowClientEntity.MailBox.Id}");
-        }
-
-        Logger.Info("On Full Sync Msg");
-        ClientGlobal.ShadowClientEntity.FromSyncContent(propertyFullSyncMsg.PropertyTree);
-
-        var ack = new PropertyFullSyncAck
-        {
-            EntityId = ClientGlobal.ShadowClientEntity.MailBox.Id,
-        };
-        Client.Instance.Send(ack);
     }
 }
---
+++ LPS.Client/StartUpManager.cs
@@ -0,0 +1,142 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="StartUpManager.cs" company="Little Princess Studio">
+// Copyright (c) Little Princess Studio. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LPS.Client;
+
+using Google.Protobuf;
+using LPS.Client.Entity;
+using LPS.Client.Rpc;
+using LPS.Common.Debug;
+using LPS.Common.Rpc;
+using LPS.Common.Rpc.InnerMessages;
+using LPS.Common.Rpc.InnerMessages.ProtobufDefs;
+
+/// <summary>
+/// Start up manager for client.
+/// </summary>
+public class StartUpManager
+{
+    private static Action<ShadowClientEntity> createShadowEntityCallBack = null!;
+    private static Func<ShadowClientEntity> getShadowEntityCallBack = null!;
+
+    /// <summary>
+    /// Init client enviroment.
+    /// </summary>
+    /// <param name="ip">Server ip to connect.</param>
+    /// <param name="port">Port to connect.</param>
+    /// <param name="entityNamespace">Costume entity class namespace to scan.</param>
+    /// <param name="rpcPropertyNamespace">Costume rpc property class namespace to scan.</param>
+    /// <param name="getShadowEntity">Function to get current client shadow entity.</param>
+    /// <param name="onCreateShadowEntity">Callback when shadow entity created.</param>
+    public static void Init(
+        string ip,
+        int port,
+        string entityNamespace,
+        string rpcPropertyNamespace,
+        Func<ShadowClientEntity> getShadowEntity,
+        Action<ShadowClientEntity> onCreateShadowEntity)
+    {
+        getShadowEntityCallBack = getShadowEntity;
+        createShadowEntityCallBack = onCreateShadowEntity;
+
+        Logger.Init("client");
+        RpcProtobufDefs.Init();
+        RpcHelper.ScanRpcMethods("LPS.Client.Entity");
+        RpcHelper.ScanRpcMethods(entityNamespace);
+        RpcHelper.ScanRpcPropertyContainer(rpcPropertyNamespace);
+
+        Client.Instance.Init(ip, port);
+
+        // Client.Instance.Init("52.175.74.209", 11001);
+        Client.Instance.RegisterMessageHandler(PackageType.ClientCreateEntity, HandleClientCreateEntity);
+        Client.Instance.RegisterMessageHandler(PackageType.EntityRpc, HandleEntityRpc);
+        Client.Instance.RegisterMessageHandler(PackageType.PropertyFullSync, HandlePropertyFullSync);
+        Client.Instance.RegisterMessageHandler(PackageType.PropertySyncCommandList, HandlePropertySyncCommandList);
+    }
+
+    /// <summary>
+    /// Start client.
+    /// </summary>
+    public static void StartClient() => Client.Instance.Start();
+
+    /// <summary>
+    /// Stop client and wait for exiting.
+    /// </summary>
+    public static void StopClient()
+    {
+        Client.Instance.Stop();
+        Client.Instance.WaitForExit();
+    }
+
+    private static void HandlePropertySyncCommandList(object arg)
+    {
+        var (msg, _, _) = ((IMessage, Connection, uint))arg;
+        var syncCommandList = (msg as PropertySyncCommandList)!;
+
+        Logger.Debug($"[HandlePropertySyncCommandList] {msg}");
+
+        getShadowEntityCallBack().ApplySyncCommandList(syncCommandList);
+    }
+
+    private static void HandleEntityRpc(object arg)
+    {
+        var (msg, _, _) = ((IMessage, Connection, uint))arg;
+        var entityRpc = (EntityRpc)msg;
+
+        // Logger.Info($"rpc msg from server {entityRpc}");
+        RpcHelper.CallLocalEntity(getShadowEntityCallBack(), entityRpc);
+    }
+
+    private static void HandleClientCreateEntity(object arg)
+    {
+        var (msg, _, _) = ((IMessage, Connection, uint))arg;
+        var clientCreateEntity = (ClientCreateEntity)msg;
+
+        Logger.Info(
+            $"Client create entity: {clientCreateEntity.EntityClassName} " +
+            $"{clientCreateEntity.ServerClientMailBox}");
+
+        var shadowEntity = RpcClientHelper.CreateClientEntity(clientCreateEntity.EntityClassName);
+        shadowEntity.OnSend = rpc => { Client.Instance.Send(rpc); };
+        shadowEntity.MailBox = RpcHelper.PbMailBoxToRpcMailBox(clientCreateEntity.ServerClientMailBox);
+        shadowEntity.BindServerMailBox();
+
+        createShadowEntityCallBack(shadowEntity);
+
+        Logger.Info($"{shadowEntity} created success.");
+
+        var requireFullSync = new RequirePropertyFullSync()
+        {
+            EntityId = shadowEntity.MailBox.Id,
+        };
+
+        Client.Instance.Send(requireFullSync);
+        Logger.Info($"require full property sync");
+    }
+
+    private static void HandlePropertyFullSync(object arg)
+    {
+        var (msg, _, _) = ((IMessage, Connection, uint))arg;
+        var propertyFullSyncMsg = (PropertyFullSync)msg;
+
+        var shadowEntity = getShadowEntityCallBack();
+
+        if (propertyFullSyncMsg.EntityId != shadowEntity.MailBox.Id)
+        {
+            throw new Exception(
+                $"Invalid property full sync {propertyFullSyncMsg.EntityId} {shadowEntity.MailBox.Id}");
+        }
+
+        Logger.Info("On Full Sync Msg");
+        shadowEntity.FromSyncContent(propertyFullSyncMsg.PropertyTree);
+
+        var ack = new PropertyFullSyncAck
+        {
+            EntityId = shadowEntity.MailBox.Id,
+        };
+        Client.Instance.Send(ack);
+    }
+}
