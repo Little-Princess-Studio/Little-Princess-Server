78f648f4cfb74da1a34c570548f8b7bc13c4a288
Yi Huang <yihuang1@microsoft.com>
2023-04-18 02:17:44 +0800

    [chore] update stop script

--- scripts/force_stop_server.sh
+++ scripts/force_stop_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pkill -9 LPS || echo "Process was not running."
+sudo -S pkill -9 LPS || echo "Process was not running."
+sudo -S pkill -9 dotnet watch run || echo "Process was not running."
