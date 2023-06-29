9501295b6facb48d298a61308d0ed38134934027
Huisama <yuwenhuisama@outlook.com>
2023-06-29 16:01:03 +0800

    [chore] update deps scripts

--- scripts/start_deps.bat
+++ scripts/start_deps.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo Start RabbitMQ...
-docker run -it --name lps-rabbitmq -d -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
+docker run --rm -it --name lps-rabbitmq -d -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
 
 echo Start Redis...
-docker run -it --name lps-redis -d redis
+docker run --rm -it --name lps-redis -d redis
 
 echo Command completed.
 pause
