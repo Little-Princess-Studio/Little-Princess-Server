64fd1d4ef123690dae067b918a4ada91a385cae6
Huisama <yuwenhuisama@oulook.com>
2023-07-02 15:16:09 +0800

    [chore] update deps scripts

--- scripts/start_deps.sh
+++ scripts/start_deps.sh
@@ -4,6 +4,9 @@ echo Start RabbitMQ...
 docker start lps-rabbitmq || docker run -it --name lps-rabbitmq -d -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
 
 echo Start Redis...
-docker start lps-redis || docker run --rm -it --name lps-redis -d -p 6379:6379 redis
+docker start lps-redis || docker run -it --name lps-redis -d -p 6379:6379 redis
+
+echo Start MongoDb...
+docker start lps-mongo || docker run -it --name lps-mongo -d -p 27017:27017 mongo:latest
 
 echo Command completed.
